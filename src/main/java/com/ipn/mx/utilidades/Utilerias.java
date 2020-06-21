d99da26786f48b5b1fc3250f796a89cd133661a0
Marcos López <faronienm@gmail.com>
2020-06-21 04:06:03 -0500

    Corección E-mail

--- src/main/java/com/ipn/mx/utilidades/Utilerias.java
+++ src/main/java/com/ipn/mx/utilidades/Utilerias.java
@@ -1,7 +1,9 @@
 package com.ipn.mx.utilidades;
 
 import java.util.Properties;
+import javax.mail.Authenticator;
 import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -25,17 +27,26 @@ public class Utilerias {
             p.put("mail.smtp.starttls.enable", "true");
             p.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
-            Session s = Session.getInstance(p);
+            Session s = Session.getInstance(p, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication("protocobas@gmail.com", "pr0t0c0l0");
+                }
+            });
+            
+            
             MimeMessage mensaje = new MimeMessage(s);
             mensaje.setFrom(new InternetAddress("protocobas@gmail.com"));
-            mensaje.addRecipients(Message.RecipientType.TO, correoDestinatario);
+            mensaje.setRecipients(Message.RecipientType.TO, InternetAddress.parse(correoDestinatario));
             mensaje.setSubject(asunto);
             mensaje.setText(texto);
-
-            Transport t = s.getTransport("smtp");
-            t.connect("protocobas@gmail.com", "pr0t0c0l0");
-            t.sendMessage(mensaje, mensaje.getAllRecipients());
-            t.close();
+            
+            Transport.send(mensaje);
+//
+//            Transport t = s.getTransport("smtp");
+//            t.connect("protocobas@gmail.com", "pr0t0c0l0");
+//            t.sendMessage(mensaje, mensaje.getAllRecipients());
+//            t.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
